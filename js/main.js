50efa23327119026c40211df3b24bc6ea9728caf
Cote <majovardon@gmail.com>
2018-07-04 11:16:19 -0400

    Cambios mínimos

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
+window.onload = () => {
 document.getElementById('btnEnter').addEventListener('click', () =>{
-  let password = document.getElementById('inlineFormInputGroupUsername2').value;
-  if(password.length <= 8 && typeof password === 'number'){
+  let password = document.getElementById('userPassword').value;
+  if(password.length <= 8 && typeof parseInt(password) === 'number'){
     document.getElementById('options').style.display = 'block';
     document.getElementById('logIn').style.display = 'none';
   } else {
-    alert('La contraseña debe tener máximo 8 caracteres y aceptar solo números.')
+    alert('La contraseña debe tener máximo 8 caracteres y acepta sólo números.')
   }
 })
+const user = document.getElementById('userPassword').value;
+window.localStorage.setItem('User', user);
+console.log('User')
+}
+
 //Ésta promesa se activa cuando se apreta el botón de btnEnter.
-fetch('http://www.psep.cl/api/Bip.php?&numberBip=666')
+/*fetch('http://www.psep.cl/api/Bip.php?&numberBip=666')
 .then(response => response.json())
 .then(dataJSON => {
   card = dataJSON;
@@ -22,4 +28,4 @@ cardData = () =>{
   for (var i in card) {
     console.log(i); 
  }
-}
+}*/
